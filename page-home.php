d8e0fb9d17851798ccff9a9e8013534098ee8178
Pablo Ramirez <pabloramirez@192.168.1.7>
2022-11-30 12:57:26 -0600

    new hero

--- page-home.php
+++ page-home.php
@@ -8,8 +8,7 @@ get_header(); ?>
 <div class="hero">
     <div class="hero_container">
         <div class="hero_container_txt">
-            <h1>Colchonpedia</h1>
-            <h2>Tu centro de información sobre colchones</h2>
+            <h1>Tu centro de información sobre colchones</h1>
             <p>
                 Te ayudamos a encontrar el mejor colchón que se adapte a tus
                 necesidades y te ofrezca el mejor descanso.
