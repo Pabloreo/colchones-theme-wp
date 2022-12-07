7ce490f50f153bdc72efd6a84c1ee968833b48cb
Pablo Ramirez <pabloramirez@Gabriels-MacBook-Air.local>
2022-12-07 16:29:24 -0600

    best

--- page-home.php
+++ page-home.php
@@ -22,14 +22,40 @@ get_header(); ?>
 </div>
 
 <section class="best">
-    <div>
-
+    <div class="best-txt">
+        <h2>Los mejores colchones en México en 2022</h2>
+        <h1>El colchón ganador 2022</h1>
+        <h1>Nezt</h1>
+        <ul>
+            <li>Soporte ortopédico.</li>
+            <li>Marca 100% mexicana.</li>
+            <li>Tecnología AirFlow con flujo de aire.</li>
+            <li>Certiﬁcación mundial CertiPUR-US®.</li>
+            <li>Hipoalergénico y antiácaros.</li>
+            <li>101 noches de prueba y envío gratis.</li>
+            <li>Dos líneas: Nezt (premium, semi ﬁrme) y Element (gran precio, ﬁrme).</li>
+        </ul>
+    </div>
+    <div class="best-testimonials">
+        <h1>El mejor colchón que he tenido</h1>
+        <div class="best-testimonials-stars">
+            <img src="https://colchones.mx/wp-content/uploads/2022/12/estrellita.png" alt="Estrellas mejor colchón">
+            <img src="https://colchones.mx/wp-content/uploads/2022/12/estrellita.png" alt="Estrellas mejor colchón">
+            <img src="https://colchones.mx/wp-content/uploads/2022/12/estrellita.png" alt="Estrellas mejor colchón">
+            <img src="https://colchones.mx/wp-content/uploads/2022/12/estrellita.png" alt="Estrellas mejor colchón">
+            <img src="https://colchones.mx/wp-content/uploads/2022/12/estrellita.png" alt="Estrellas mejor colchón">
+        </div>
+        <div class="best-testimonials">
+            <h3>Excelente</h3>
+            <p>Necesitaba un colchón que me permitiera descansar porque necesito soporte lumbar. Después de buscar opciones y leer reseñas me decidí por el colchón Nezt Element Firme. Me sorprendió que llegara en caja, lo sacamos de su plástico e instalamos fácilmente. Desde esa noche despierto sin dolores en la espalda. ¡Excelente producto, lo recomiendo ampliamente!</p>
+            <h2>- Raymundo L.</h2>
+        </div>
     </div>
 </section>
 
 <div style="height: 150px; overflow: hidden;">
     <svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
-        <path d="M-7.05,-14.28 C112.58,336.03 232.78,-229.42 505.36,108.06 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #e37b75;"></path>
+        <path d="M-7.05,-14.28 C112.58,336.03 232.78,-229.42 505.36,108.06 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #ff5a62;"></path>
     </svg>
 </div>
 
