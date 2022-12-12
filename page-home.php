7c9b470aa9ec4ee2b08bec793f3967406d66e7ad
Pablo Ramirez <pabloramirez@Gabriels-MacBook-Air.local>
2022-12-12 14:25:14 -0600

    contendientes

--- page-home.php
+++ page-home.php
@@ -53,11 +53,47 @@ get_header(); ?>
     </div>
 </section>
 
-<div style="height: 150px; overflow: hidden;">
-    <svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
-        <path d="M-7.05,-14.28 C112.58,336.03 232.78,-229.42 505.36,108.06 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #ff5a62;"></path>
-    </svg>
-</div>
+<section>
+    <div style="height: 150px; overflow: hidden;">
+        <svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+            <path d="M-7.05,-14.28 C112.58,336.03 232.78,-229.42 505.36,108.06 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #ff5a62;"></path>
+        </svg>
+    </div>
+    <div class="contendientes">
+        <div class="contendientes-content">
+            <h1>Contendientes</h1>
+
+            <div class="contendiente">
+                <h1>EMMA</h1>
+                <ul>
+                    <li>-Soporte óptimo.</li>
+                    <li>-Aísla el movimiento.</li>
+                    <li>-Estructura con diferentes capas de espuma.</li>
+                </ul>
+                <div class="contendiente-testimonials">
+                    <h1>El mejor colchón que he tenido</h1>
+                    <div class="contendiente-testimonials-stars">
+                        <img src="https://colchones.mx/wp-content/uploads/2022/12/estrbl.png" alt="Estrellas">
+                        <img src="https://colchones.mx/wp-content/uploads/2022/12/estrbl.png" alt="Estrellas">
+                        <img src="https://colchones.mx/wp-content/uploads/2022/12/estrbl.png" alt="Estrellas">
+                        <img src="https://colchones.mx/wp-content/uploads/2022/12/estrbl.png" alt="Estrellas">
+                        <img src="https://colchones.mx/wp-content/uploads/2022/12/mediaestrella.png" alt="Estrellas">
+                    </div>
+                    <div class="contendiente-testimonials">
+                        <h3>4.3 Muy bueno</h3>
+                        <p>Es muy blandito comparado con uno de resortes ortopédico que tengo en otro
+                            cuarto, se hunde muchísimo, no sé si lo utilizaría de diario pero para el fin de
+                            semana que lo uso duermo excelente. Muy cómodo hasta el momento. Tardó
+                            un poco en llegar y la caja estaba algo maltratada.</p>
+                        <h2>- Marco Antonio R.</h2>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+</section>
 
 
 <?php get_footer(); ?>
