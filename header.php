05537307550168d7a513b67db5b589c76ff1b4c5
Pablo Ramirez <pabloramirez@Gabriels-MacBook-Air.local>
2022-12-21 15:17:47 -0600

    GA4

--- header.php
+++ header.php
@@ -1,13 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/b812c8ce06.js" crossorigin="anonymous"></script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-N9SHECH2QH"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-N9SHECH2QH');
+    </script>
+
     <title>Colchones.mx</title>
     <?php wp_head(); ?>
 </head>
+
 <body>
     <header class="site-header">
         <div class="contenedor">
@@ -18,14 +33,13 @@
                     </a>
                 </div>
                 <?php
-                    $args = array(
-                        'theme_location' => 'menu-principal',
-                        'container' => 'nav',
-                        'container_class' => 'menu-principal'
-                    );
-                    wp_nav_menu($args); 
-                    ?>
+                $args = array(
+                    'theme_location' => 'menu-principal',
+                    'container' => 'nav',
+                    'container_class' => 'menu-principal'
+                );
+                wp_nav_menu($args);
+                ?>
             </div>
         </div>
     </header>
-
